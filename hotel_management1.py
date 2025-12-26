ac43be47d91ef748cea0274975c96f3152c06441
shellikerishukla-04dot <shellikerishukla@gmail.com>
2025-12-26 21:29:15 +0530

    Update hotel_management1.py

--- hotel_management1.py
+++ hotel_management1.py
@@ -1,15 +1,18 @@
 import sys
 
-if len(sys.argv) < 4:
-    name = sys.argv[1]
-    room = sys.argv[2]
-    nights = int(sys.argv[3])
-    if room == "Standard":
-        bill = 1000 * nights
-    elif room == "Deluxe":
-        bill = 2000 * nights
-    else:
-        bill = 3000 * nights
-        print("name =", name)
-        print("room =", room)
-        print("bill =", bill)
+# Get command-line arguments
+name = sys.argv[1]
+room = sys.argv[2]
+nights = int(sys.argv[3])
+
+# Calculate bill using if-else
+if room == "Standard":
+    bill = 1000 * nights
+elif room == "Deluxe":
+    bill = 2000 * nights
+else:
+    bill = 3000 * nights
+
+print("name =", name)
+print("room =", room)
+print("bill =", bill)
