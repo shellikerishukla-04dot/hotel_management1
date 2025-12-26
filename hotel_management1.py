7192ba8aff4c5eff0d227ad5774f8821125fc853
shellikerishukla-04dot <shellikerishukla@gmail.com>
2025-12-26 21:34:01 +0530

    Update hotel_management1.py

--- hotel_management1.py
+++ hotel_management1.py
@@ -1,9 +1,6 @@
 import sys
 
-# Check argument count
-if len(sys.argv) != 4:
-    print("Usage: python hotel_management.py <guest_name> <room_type> <nights>")
-    sys.exit()
+
 
 guest_name = sys.argv[1]
 room_type = sys.argv[2]
