2dca3e9ca0c528ebdb9845abe5387ffa437d9157
shellikerishukla-04dot <shellikerishukla@gmail.com>
2025-12-26 22:02:07 +0530

    Create test_hotel_management1.py

---
+++ test_hotel_management1.py
@@ -0,0 +1,10 @@
+from hotel_management1 import calculate_bill
+
+def test_standard_room():
+    assert calculate_bill("Standard", 2) == 2000
+
+def test_deluxe_room():
+    assert calculate_bill("Deluxe", 3) == 6000
+
+def test_suite_room():
+    assert calculate_bill("Suite", 1) == 3000
