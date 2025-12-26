d6c67e6a3021dea383a0f7bb78a22a32467ecd25
shellikerishukla-04dot <shellikerishukla@gmail.com>
2025-12-26 22:14:20 +0530

    Update test_hotel_management1.py

--- test_hotel_management1.py
+++ test_hotel_management1.py
@@ -1,10 +1,10 @@
 from hotel_management1 import bill
 
 def test_standard_room():
-    assert calculate_bill("Standard", 2) == 2000
+    assert bill("Standard", 2) == 2000
 
 def test_deluxe_room():
-    assert calculate_bill("Deluxe", 3) == 6000
+    assert bill("Deluxe", 3) == 6000
 
 def test_suite_room():
-    assert calculate_bill("Suite", 1) == 3000
+    assert bill("Suite", 1) == 3000
